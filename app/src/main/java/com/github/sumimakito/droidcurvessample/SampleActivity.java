24c0407f1ce9e9762e3b2f3bc7c5f557099c9a45
SumiMakito <sumimakito@hotmail.com>
2016-08-24 14:05:19 +0800

    Dirty-fix: expand the curve so that it will not end too early inside the visible area; add more style attributes; add style setters in Java code; add an interface to let users receive changed values;

--- app/src/main/java/com/github/sumimakito/droidcurvessample/SampleActivity.java
+++ app/src/main/java/com/github/sumimakito/droidcurvessample/SampleActivity.java
@@ -8,13 +8,49 @@ package com.github.sumimakito.droidcurvessample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import com.github.sumimakito.droidcurves.CurvesView;
 import com.github.sumimakito.droidcurvessample.R;
 
 public class SampleActivity extends Activity {
+    private CurvesView curvesView;
+    private TextView textView1, textView2, textView3, textView4, textView5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
+
+        curvesView = (CurvesView) findViewById(R.id.curvesView);
+        textView1 = (TextView) findViewById(R.id.textView1);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (TextView) findViewById(R.id.textView3);
+        textView4 = (TextView) findViewById(R.id.textView4);
+        textView5 = (TextView) findViewById(R.id.textView5);
+
+        curvesView.setCurvesChangedListener(new CurvesView.CurvesChangedListener() {
+            @Override
+            public void onCallback(int controlKnotID, float newValue) {
+                switch (controlKnotID) {
+                    case 0:
+                        textView1.setText(Math.round(newValue * 100) + "%");
+                        break;
+                    case 1:
+                        textView2.setText(Math.round(newValue * 100) + "%");
+                        break;
+                    case 2:
+                        textView3.setText(Math.round(newValue * 100) + "%");
+                        break;
+                    case 3:
+                        textView4.setText(Math.round(newValue * 100) + "%");
+                        break;
+                    case 4:
+                        textView5.setText(Math.round(newValue * 100) + "%");
+                        break;
+                }
+            }
+        });
     }
 }
--- library/src/main/java/com/github/sumimakito/droidcurves/CurvesView.java
+++ library/src/main/java/com/github/sumimakito/droidcurves/CurvesView.java
@@ -17,6 +17,7 @@ import android.graphics.PointF;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
@@ -27,15 +28,15 @@ import java.util.List;
 public class CurvesView extends View {
     private final String TAG = "CurvesView";
 
-    private int cvBackgroundColor, cvCurveColor, cvHintLineColor, cvTextColor;
+    private int cvBackgroundColor, cvCurveColor, cvHintLineColor, cvTextColor, cvTouchEffectColor;
 
-    private int cvCtrlPointCount, cvTextSize;
+    private int cvCtrlKnotCount, cvTextSize;
     private float cvCurveWeight, cvHintLineWeight;
-    private boolean cvDrawText;
-    private int cvTextTypeface;
+    private boolean cvDrawText, cvDrawTouchEffect;
+    private Typeface cvTextTypeface;
 
     private float columnMaskWidth = 0, columnMaskHeight = 0;
-    private int pointColumnAreaIndex = -1;
+    private int opKnotIdx = -1;
     private float startPointY = -1;
 
     private ArrayList<PointF> knotsArray;
@@ -44,6 +45,9 @@ public class CurvesView extends View {
     private Paint pCurve, pMask, pLine, pText;
     private int viewWidth, viewHeight;
     private float textLabelGlobalY;
+    private CurvesChangedListener curvesChangedListener;
+
+    private float _dirtyFixXOffset;
 
     public CurvesView(Context context) {
         this(context, null, 0);
@@ -57,6 +61,7 @@ public class CurvesView extends View {
         super(context, attrs, defStyleAttr);
         applyStyle(context, attrs, defStyleAttr);
         initArrays();
+        reloadPaints();
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -64,6 +69,7 @@ public class CurvesView extends View {
         super(context, attrs, defStyleAttr, defStyleRes);
         applyStyle(context, attrs, defStyleAttr);
         initArrays();
+        reloadPaints();
     }
 
     @Override
@@ -82,39 +88,48 @@ public class CurvesView extends View {
         drawSplineCubicHermite(knotsArray, 0.05f, canvas, pCurve);
 
         if (cvDrawText) {
-            for (int i = 0; i < cvCtrlPointCount; i++) {
+            for (int i = 0; i < cvCtrlKnotCount; i++) {
                 canvas.drawText(Math.round(valuesArray.get(i) * 100) + "%", (i + 0.5f) * columnMaskWidth, textLabelGlobalY, pText);
             }
         }
 
-        if (pointColumnAreaIndex >= 0) {
-            canvas.drawRect(pointColumnAreaIndex * columnMaskWidth, 0,
-                            (pointColumnAreaIndex + 1) * columnMaskWidth, columnMaskHeight,
+        if (cvDrawTouchEffect && opKnotIdx >= 0) {
+            canvas.drawRect(opKnotIdx * columnMaskWidth, 0,
+                            (opKnotIdx + 1) * columnMaskWidth, columnMaskHeight,
                             pMask);
         }
     }
 
     private void applyStyle(Context context, AttributeSet attrs, int defStyleAttr) {
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CurvesView, defStyleAttr, 0);
+
         cvBackgroundColor = a.getColor(R.styleable.CurvesView_cv_backgroundColor, Color.BLACK);
+        cvTouchEffectColor = a.getColor(R.styleable.CurvesView_cv_touchEffectColor, Color.argb(30, 255, 255, 255));
         cvCurveColor = a.getColor(R.styleable.CurvesView_cv_curveColor, Color.WHITE);
         cvHintLineColor = a.getColor(R.styleable.CurvesView_cv_hintLineColor, Color.argb(50, 255, 255, 255));
         cvTextColor = a.getColor(R.styleable.CurvesView_cv_textColor, Color.WHITE);
+
         cvDrawText = a.getBoolean(R.styleable.CurvesView_cv_drawText, true);
-        cvCtrlPointCount = a.getInt(R.styleable.CurvesView_cv_ctrlPointCount, 5);
+        cvDrawTouchEffect = a.getBoolean(R.styleable.CurvesView_cv_drawTouchEffect, true);
+
+        cvCtrlKnotCount = a.getInt(R.styleable.CurvesView_cv_ctrlPointCount, 5);
+
         cvCurveWeight = a.getDimensionPixelSize(R.styleable.CurvesView_cv_curveWeight,
                                                 Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 3, getResources().getDisplayMetrics())));
         cvHintLineWeight = a.getDimensionPixelSize(R.styleable.CurvesView_cv_hintLineWeight,
                                                    Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics())));
         cvTextSize = a.getDimensionPixelSize(R.styleable.CurvesView_cv_textSize,
                                              Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 11, getResources().getDisplayMetrics())));
-        cvTextTypeface = a.getInt(R.styleable.CurvesView_cv_textTypeface, 0);
+        int cvTypefaceID = a.getInt(R.styleable.CurvesView_cv_textTypeface, 0);
+        cvTextTypeface = (cvTypefaceID == 0 ? Typeface.MONOSPACE : cvTypefaceID == 1 ? Typeface.DEFAULT : Typeface.DEFAULT_BOLD);
         a.recycle();
+    }
 
+    private void reloadPaints() {
         pText = new Paint();
         pText.setColor(cvTextColor);
         pText.setAntiAlias(true);
-        pText.setTypeface(cvTextTypeface == 0 ? Typeface.MONOSPACE : cvTextTypeface == 1 ? Typeface.DEFAULT : Typeface.DEFAULT_BOLD);
+        pText.setTypeface(cvTextTypeface);
         pText.setTextSize(cvTextSize);
         pText.setTextAlign(Paint.Align.CENTER);
 
@@ -134,16 +149,15 @@ public class CurvesView extends View {
         pLine.setStrokeWidth(cvHintLineWeight);
 
         pMask = new Paint();
-        pMask.setColor(Color.WHITE);
+        pMask.setColor(cvTouchEffectColor);
         pMask.setAntiAlias(true);
-        pMask.setAlpha(30);
     }
 
     private void initArrays() {
         knotsArray = new ArrayList<>();
         valuesArray = new ArrayList<>();
-        float valueInterval = 1.0f / (cvCtrlPointCount - 1);
-        for (int i = 0; i < cvCtrlPointCount; i++) {
+        float valueInterval = 1.0f / (cvCtrlKnotCount - 1);
+        for (int i = 0; i < cvCtrlKnotCount; i++) {
             valuesArray.add(i * valueInterval);
         }
     }
@@ -151,18 +165,20 @@ public class CurvesView extends View {
     private void rescaleView(int width, int height) {
         viewWidth = width;
         viewHeight = height;
-        columnMaskWidth = (float) width / (float) cvCtrlPointCount;
+        //Log.w(TAG, "W@" + width + ", H@" + height);
+        columnMaskWidth = (float) width / (float) cvCtrlKnotCount;
         columnMaskHeight = height;
+        _dirtyFixXOffset = viewWidth * 0.125f;
         knotsArray.clear();
 
         // initial curve
         double hypotenuse = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
         double cos = (double) width / hypotenuse;
         double sin = (double) height / hypotenuse;
-        double pOffsetX = hypotenuse / ((double) (cvCtrlPointCount - 1)) * cos;
-        double pOffsetY = hypotenuse / ((double) (cvCtrlPointCount - 1)) * sin;
+        double pOffsetX = hypotenuse / ((double) (cvCtrlKnotCount - 1)) * cos;
+        double pOffsetY = hypotenuse / ((double) (cvCtrlKnotCount - 1)) * sin;
 
-        for (int i = 0; i < cvCtrlPointCount; i++) {
+        for (int i = 0; i < cvCtrlKnotCount; i++) {
             knotsArray.add(
                     new PointF(
                             Math.round(pOffsetX * i),
@@ -180,23 +196,25 @@ public class CurvesView extends View {
             case MotionEvent.ACTION_DOWN:
                 float x = event.getX();
                 startPointY = event.getY();
-                pointColumnAreaIndex = (int) Math.floor(x / columnMaskWidth);
+                opKnotIdx = (int) Math.floor(x / columnMaskWidth);
                 invalidate();
                 break;
             case MotionEvent.ACTION_MOVE:
                 float deltaY = event.getY() - startPointY; // ↓:>0 ↑:<0
-                float targetValue = valuesArray.get(pointColumnAreaIndex) - deltaY * 0.0001f;
+                float targetValue = valuesArray.get(opKnotIdx) - deltaY * 0.0001f;
                 if (targetValue < 0.0f)
                     targetValue = 0.0f;
                 else if (targetValue > 1.0f)
                     targetValue = 1.0f;
-                valuesArray.set(pointColumnAreaIndex, targetValue);
-                knotsArray.get(pointColumnAreaIndex).y = columnMaskHeight * (1.0f - targetValue); //+= deltaY * 0.1;
+                valuesArray.set(opKnotIdx, targetValue);
+                knotsArray.get(opKnotIdx).y = columnMaskHeight * (1.0f - targetValue); //+= deltaY * 0.1;
 
+                if (curvesChangedListener != null)
+                    curvesChangedListener.onCallback(opKnotIdx, targetValue);
                 invalidate();
                 break;
             case MotionEvent.ACTION_UP:
-                pointColumnAreaIndex = -1;
+                opKnotIdx = -1;
                 startPointY = -1;
                 invalidate();
                 break;
@@ -207,10 +225,17 @@ public class CurvesView extends View {
     private void drawSplineCubicHermite(List<PointF> knots, float delta, Canvas canvas, Paint paint) {
         Path path = new Path();
         int n = knots.size();
-        float pX = 0;
-        float pY = 0;
+        float pX = 0, pY = 0;
+        float lX0 = 0, lY0 = 0, lX1 = 0, lY1 = 0;
+        int lFlg = -1;
         for (int i = 0; i < n; i++) {
             for (float t = 0; t < 1; t += delta) {
+
+                if (lFlg == -1 && i == (n - 1) && t >= 0.5f) {
+                    //Log.w(TAG, "set Flag to 0");
+                    lFlg = 0;
+                }
+
                 float h00 = (1 + 2 * t) * (1 - t) * (1 - t);
                 float h10 = t * (t - 1) * (t - 1);
                 float h01 = t * t * (3 - 2 * t);
@@ -264,8 +289,29 @@ public class CurvesView extends View {
                             * m1.y;
                     path.lineTo(pX, limitY(pY));
                 }
+
+                if (lFlg == 0) {
+                    lX0 = pX;
+                    lY0 = limitY(pY);
+                    lFlg = 1;
+                    //Log.w(TAG, "set Flag to 1; lX0=" + lX0 + ", lY0=" + lY0);
+                } else if (lFlg == 1) {
+                    lX1 = pX;
+                    lY1 = limitY(pY);
+                    lFlg = 2;
+                    //Log.w(TAG, "set Flag to 2; lX1=" + lX1 + ", lY1=" + lY1);
+                }
             }
         }
+
+        /*
+         * Dirty-fix: expand line
+         */
+
+        float dfX = _dirtyFixXOffset;
+        float dfY = dfX * (lY1 - lY0) / (lX1 - lX0);
+        path.lineTo(lX1 + dfX, lY1 + dfY);
+
         canvas.drawPath(path, paint);
     }
 
@@ -277,5 +323,78 @@ public class CurvesView extends View {
         if (pY > columnMaskHeight - cvCurveWeight) return columnMaskHeight - cvCurveWeight;
         else if (pY < cvCurveWeight) return cvCurveWeight;
         else return pY;
+    }
+
+
+    public void setCurvesChangedListener(CurvesChangedListener listener) {
+        this.curvesChangedListener = listener;
+    }
+
+    public void setCvBackgroundColor(int cvBackgroundColor) {
+        this.cvBackgroundColor = cvBackgroundColor;
+        reloadPaints();
+        invalidate();
+    }
+
+    public void setCvCurveColor(int cvCurveColor) {
+        this.cvCurveColor = cvCurveColor;
+        reloadPaints();
+        invalidate();
+    }
+
+    public void setCvCurveWeight(float cvCurveWeight) {
+        this.cvCurveWeight = cvCurveWeight;
+        reloadPaints();
+        invalidate();
+    }
+
+    public void setCvDrawText(boolean cvDrawText) {
+        this.cvDrawText = cvDrawText;
+        invalidate();
+    }
+
+    public void setCvDrawTouchEffect(boolean cvDrawTouchEffect) {
+        this.cvDrawTouchEffect = cvDrawTouchEffect;
+        invalidate();
+    }
+
+    public void setCvHintLineColor(int cvHintLineColor) {
+        this.cvHintLineColor = cvHintLineColor;
+        reloadPaints();
+        invalidate();
+    }
+
+    public void setCvHintLineWeight(float cvHintLineWeight) {
+        this.cvHintLineWeight = cvHintLineWeight;
+        reloadPaints();
+        invalidate();
+    }
+
+    public void setCvTextColor(int cvTextColor) {
+        this.cvTextColor = cvTextColor;
+        reloadPaints();
+        invalidate();
+    }
+
+    public void setCvTextSize(int cvTextSize) {
+        this.cvTextSize = cvTextSize;
+        reloadPaints();
+        invalidate();
+    }
+
+    public void setCvTouchEffectColor(int cvTouchEffectColor) {
+        this.cvTouchEffectColor = cvTouchEffectColor;
+        reloadPaints();
+        invalidate();
+    }
+
+    public void setCvTextTypeface(Typeface cvTextTypeface) {
+        this.cvTextTypeface = cvTextTypeface;
+        reloadPaints();
+        invalidate();
+    }
+
+    public interface CurvesChangedListener {
+        void onCallback(int controlKnotID, float newValue);
     }
 }
